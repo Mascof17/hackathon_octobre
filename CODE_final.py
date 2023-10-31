8efcb51e0987fea9695f99669bbc36ecdd4e5f32
Mascof17 <maxdebussac@gmail.com>
2023-10-31 18:06:07 +0100

    On regrouoe tout dans un même fichier python

--- CODE_final.py
+++ CODE_final.py
@@ -80,8 +80,9 @@ BOARD_8_9[::7, 1::6] = 1
 SMALL_BOARD = np.array([[1, 0, 1], [0, 0, 0], [1, 0, 0]], dtype=DTYPE)
 SMALL_PIECE = np.array([[0, 1], [1, 1]], dtype=DTYPE)
 
+#NOUS GENERONS DESORMAIS TOUTES LES FIGURES CORRESPONDANT A TOUTES LES LETTRES ROTATEES ET REFLECHIES
 
-
+#gros problème ici que je ne comprends toujours pas donc je tente un autre tri moins propre
 # def positions(piece):
 #     rotat1= np.rot90(RAW_SHAPES[piece][0],k=1)
 #     rotat2= np.rot90(RAW_SHAPES[piece][0],k=2)
@@ -98,6 +99,8 @@ SMALL_PIECE = np.array([[0, 1], [1, 1]], dtype=DTYPE)
     #         vu[tuple(element)] = True
     #         RAW_SHAPES[piece].append(element)
     # return 
+
+#la voici cette fonction
 
 def positions_2(piece):
     rotat1= np.rot90(RAW_SHAPES[piece][0],k=1)
@@ -123,13 +126,122 @@ def positions_2(piece):
 
     return 
 
-
-
-    
-
-
+#ON AJOUTE TOUTES CES FIGURES AU DICTIONNAIRE INTRODUIT PLUS AVANT
 for piece in RAW_SHAPES:
     positions_2(piece)
 
 
-print(RAW_SHAPES)
+
+
+def vérification(board, piece, x, y):
+    """
+    Renvoie True si la pièce peut être placée, False sinon.
+    """
+    piece_hauteur, piece_largeur = piece.shape
+    board_hauteur, board_largeur = board.shape
+
+    # Vérifie si la pièce peut être placée à la position (x, y)
+    if x + piece_hauteur > board_hauteur or y + piece_largeur > board_largeur:
+        return False
+
+    for i in range(piece_hauteur) :
+        for k in range(piece_largeur) :
+            if (piece[i,k] == 1 and board[x+i, y+k] == 1) : return False
+    return True
+
+
+
+
+
+def trouver_emplacements_pentamino(board, piece):
+    board_hauteur, board_largeur = board.shape
+    piece_hauteur, piece_largeur = piece.shape
+
+    emplacements = []
+
+    for x in range(board_hauteur):
+        for y in range(board_largeur):
+            if vérification(board, piece, x, y):
+                emplacements.append((x, y))
+
+    return emplacements
+
+# Exemple d'utilisation
+# board = np.array([
+#     [0, 1, 1, 1, 0],
+#     [0, 1, 1, 1, 0],
+#     [1, 0, 1, 0, 0],
+#     [1, 0, 0, 0, 0]
+# ])
+
+# piece = np.array([
+#     [1, 1],
+#     [1, 1],
+#     [1, 0]
+# ])
+
+# emplacements_possibles = trouver_emplacements_pentamino(board, piece)
+# print("Emplacements possibles pour le pentamino :")
+# print(emplacements_possibles)
+
+# à partir des emplacements possibles j'essaye de faire le tableau :
+
+#je fais une fonction qui crée le tableau pour une seule pièce
+
+#problème : faire un dictionnaire où on a rajouté les pièces pivotées de max pour qu'elles aient le même nom pour bien remplir la première partie
+def piece_unique(piece, board) :
+    board_hauteur, board_largeur = board.shape
+    emplacements_possibles = trouver_emplacements_pentamino(board, piece)
+    #on initialise le tableau nul
+    tab = np.zeros((len(emplacements_possibles), 72))
+    #on remplit la première partie pour indiquer l'indice de la pièce
+    i = RAW_SHAPES.index(piece)
+    for liste in tab : tab[liste][i] = 1
+
+    #on remplit le tableau
+    for (x,y) in emplacements_possibles :
+        for (i,k) in piece :
+            tab[(x+i)*board_longueur + (y+k)] = 1
+    return tab
+
+#puis j'additionne les tableaux en concaténant par le bas
+
+
+
+#ON IMPLEMENTE ENSUITE LA FONCTION EXACT_COVER
+
+
+# data = np.array([[1, 1,1, 0,0,0],[0,0,0,1,0,1],[0, 0,0, 1,1,1],[0,0,0,0,1,0]], dtype=np.int32)
+# expected = np.array([0, 1])
+# actual = exact_cover.get_exact_cover(data)
+
+
+
+
+
+
+def imprimer(n,L,rep,longueur,hauteur):
+    pieces=[]
+    tableau=[[0 for k in range (longueur)]for i in range(hauteur) ]
+    print(tableau)
+    for elem in rep:
+        pieces.append(L[elem])
+    print(pieces)
+    for elem in pieces:
+        num=0
+        for i in range(n):
+            if elem[i]==1:
+                num=i+1
+        print(num)
+        for k in range (n,len(L[0])):
+            if elem[k]==1:
+                tableau [((k-n)//longueur)][(k-n)%longueur]=num
+                print((k-n)//hauteur,(k-n)//hauteur,k)
+    return tableau
+l=[[1,0,0,1,1,1,0,0,0],[1,0,0,0,0,0,1,1,1],[0,1,0,0,0,0,0,1,0],[0,0,1,0,0,0,1,0,1]]
+reptest=[0,2,3]
+# print(imprimer(3,l,reptest,2,3))
+        
+
+
+#IL MANQUE ENCORE QUELQUES LIGNES POUR FAIRE LE LIEN ENTRE LES PROGRAMMES ECRITS PAR CHACUN
